fd7f7ee5e250ed42b0d388677e725535627c4882
Fernando <fherrera2018@gmail.com>
2021-09-19 15:19:15 -0700

    Small edits.

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,19 @@ import Header from "./Components/Header"
 
 function App() {
   return (
-    <div className="app">
+    <div >
       <Header />
       <div className="search">
         <form>
-          <input type="search" id="search" placeholder="Thor: Ragnarok" required />
+          <input
+            type="search"
+            id="search-movie"
+            placeholder="Thor: Ragnarok"
+            required />
           <button>Search</button>
         </form>
       </div>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
   );
 }
 
